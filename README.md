08ea529c1a4c521f5c7f9e0006397da90094d1cc
shunjid <shunjid.rahman@gmail.com>
2022-01-07 12:36:28 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shunjid
+
+test
---
+++ assets/js/plugin.js
@@ -0,0 +1,1 @@
+$(function(){$(".tabs").tabs(),$(".collapsible").collapsible(),$(".sidenav").sidenav().on("click tap","li a",()=>{$(".sidenav").sidenav("close")})}),document.addEventListener("DOMContentLoaded",function(){window.setTimeout(document.querySelector("#banner-svg").classList.add("animated"),1e3)});
